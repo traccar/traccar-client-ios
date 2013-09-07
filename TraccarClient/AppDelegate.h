f0301b31cd5aacf86b8d3f32ba84289b52fa2d08
User <user@Users-Mac.local>
2013-09-07 20:31:50 +1200

    Initial Commit

---
+++ TraccarClient/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TraccarClient
+//
+//  Created by User on 7/09/13.
+//  Copyright (c) 2013 Traccar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TraccarClient/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TraccarClient
+//
+//  Created by User on 7/09/13.
+//  Copyright (c) 2013 Traccar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
