ed25668888257785d827d1091888f827efb864c7
Anton Tananaev <anton.tananaev@gmail.com>
2015-08-26 11:13:34 +1200

    Add core data boilerplate code

--- TraccarClient/TCAppDelegate.h
+++ TraccarClient/TCAppDelegate.h
@@ -20,6 +20,10 @@
 
 @property (strong, nonatomic) UIWindow *window;
 
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
 - (void)registerDefaultsFromSettingsBundle;
 
 @end
