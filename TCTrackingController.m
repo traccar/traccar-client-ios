dc97396c62d6087cdaf489780bb76820bc95d75c
Anton Tananaev <anton.tananaev@gmail.com>
2015-08-27 17:01:44 +1200

    Add state machine logic

--- TCTrackingController.m
+++ TCTrackingController.m
@@ -18,6 +18,10 @@
 #import "TCPositionProvider.h"
 #import "TCDatabaseHelper.h"
 #import "TCNetworkManager.h"
+#import "TCProtocolFormatter.h"
+#import "TCRequestManager.h"
+
+int64_t kRetryDelay = 30 * 1000;
 
 @interface TCTrackingController () <TCPositionProviderDelegate, TCNetworkManagerDelegate>
 
@@ -27,6 +31,12 @@
 @property (nonatomic, strong) TCPositionProvider *positionProvider;
 @property (nonatomic, strong) TCDatabaseHelper *databaseHelper;
 @property (nonatomic, strong) TCNetworkManager *networkManager;
+
+- (void)write:(TCPosition *)position;
+- (void)read;
+- (void)delete:(TCPosition *)position;
+- (void)send:(TCPosition *)position;
+- (void)retry;
 
 @end
 
@@ -61,6 +71,54 @@
 
 - (void)didUpdateNetwork:(BOOL)online {
     
+}
+
+//
+// State transition examples:
+//
+// write -> read -> send -> delete -> read
+//
+// read -> send -> retry -> read -> send
+//
+
+- (void)write:(TCPosition *)position {
+    if (self.online && self.waiting) {
+        [self read];
+        self.waiting = NO;
+    }
+}
+
+- (void)read {
+    TCPosition *position = [self.databaseHelper selectPosition];
+    if (position) {
+        [self send:position];
+    } else {
+        self.waiting = YES;
+    }
+}
+
+- (void)delete:(TCPosition *)position {
+    [self.databaseHelper deletePosition:position];
+    [self read];
+}
+
+- (void)send:(TCPosition *)position {
+    NSURL *request = [TCProtocolFormatter formatPostion:position address:@"TODO" port:666]; // TODO
+    [TCRequestManager sendRequest:request completionHandler:^(BOOL success) {
+        if (success) {
+            [self delete:position];
+        } else {
+            [self retry];
+        }
+    }];
+}
+
+- (void)retry {
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, kRetryDelay * NSEC_PER_MSEC), dispatch_get_main_queue(), ^{
+        if (self.online) {
+            [self read];
+        }
+    });
 }
 
 @end
